2abbd5d0225118b09f4d2016bba63d669039b743
Kanykei <kanisatybaldyeva@gmail.com>
2023-09-02 13:43:23 +0600

    save commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <h1>Вход в trello</h1>
+      <div>
+        <input type="email" placeholder="Enter your Email..." />
+        <input type="password" placeholder="Enter your Password..." />
+      </div>
+    </div>
+  );
+};
